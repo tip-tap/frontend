1773bf3b00c4fc9b6bef7639d7e0df1b607d2dc2
dianestar <dianestar@naver.com>
2022-07-24 01:40:23 +0900

    Feat: 카카오 지도 세팅 및 생성

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./styles/main.module.scss";
 import { Route, Routes } from "react-router-dom";
 import Test from "./pages/Test";
 import Main from "./pages/Main";
+import Map from "./components/Map";
 
 function App() {
   return (
     <Routes>
       <Route path="/test" element={<Test />} />
       <Route path="/" element={<Main />} />
+      <Route path="/map" element={<Map />} />
     </Routes>
   );
 }
