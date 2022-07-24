7036aafec0439167a03aa7281cba5749f81e9d46
dianestar <dianestar@naver.com>
2022-07-25 05:29:27 +0900

    Design: Header 퍼블리싱 수정 완료

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -3,45 +3,31 @@ import styles from "../../styles/components/common/header.module.scss";
 import { ReactComponent as Logo } from "../../assets/logo.svg";
 import { ReactComponent as Map } from "../../assets/map.svg";
 import { ReactComponent as List } from "../../assets/list.svg";
-import { ReactComponent as Heart } from "../../assets/heart.svg";
-import { ReactComponent as Checklist } from "../../assets/checklist.svg";
-import { ReactComponent as Mypage } from "../../assets/mypage.svg";
 
 const Header = ({ withToggle, active }) => {
     return (
         <div className={styles.wrapper}>
-            <section>
+            <section className={styles.left}>
                 <Logo />
             </section>
             {withToggle &&
             <section className={styles.middle}>
                 <article className={active === "map" ? styles.active : styles.inactive}>
                     <Map fill={active === "map" ? "#0040BD" : "#CACACA"}/>
-                    <span>지도로 매물보기</span>
+                    <span>지도로 보기</span>
                 </article>
                 <article className={active === "list" ? styles.active : styles.inactive}>
                     <List fill={active === "list" ? "#0040BD" : "#CACACA"} />
-                    <span>리스트로 매물보기</span>
+                    <span>리스트로 보기</span>
                 </article>
             </section>
             }
             <section className={styles.right}>
-                <article>
-                    <Heart />
-                    <span>관심 매물</span>
-                </article>
-                <article>
-                    <Checklist />
-                    <span>체크리스트</span>
-                </article>
-                <article>
-                    <Checklist />
-                    <span>확정 매물</span>
-                </article>
-                <article>
-                    <Mypage />
-                    <span>마이페이지</span>
-                </article>
+                <span>관심 매물</span>
+                <span>체크리스트</span>
+                <span>확정 매물</span>
+                <span>마이페이지</span>
+                <span>로그아웃</span>
             </section>
         </div>
     )
